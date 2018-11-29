b90d302b59eed0899049486574711c0770c50dee
Matthew Sherman <msherman0801@gmail.com>
2018-11-29 15:16:50 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,13 +5,17 @@ class MP3Importer
   
   def initialize(filepath)
     @path = filepath
+<<<<<<< HEAD
     @files = []
+=======
+>>>>>>> 33a084ce989841e2edeef831e2f3137c3ec0be35
   end
   
   def files
     testPath = Dir.entries("spec/fixtures/mp3s")
     testPath.each do |i|
       if i.split('').last(4) == '.mp3'.split('')
+<<<<<<< HEAD
         @files << i.strip
       end
     end
@@ -22,5 +26,10 @@ class MP3Importer
     self.files.each do |i|
       Song.new_by_filename(i)
     end
+=======
+        bindin
+      end
+    end 
+>>>>>>> 33a084ce989841e2edeef831e2f3137c3ec0be35
   end
 end
